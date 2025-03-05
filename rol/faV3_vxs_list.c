a87db2ad90d6886fd9687b45cf67773afedcc934
Bryan Moffit <moffit@jlab.org>
2025-03-05 04:56:51 -0500

    Update rol to use config file, cleanup Makefile
    - cleanup errors and warnings from readout list compilation

--- rol/faV3_vxs_list.c
+++ rol/faV3_vxs_list.c
@@ -25,12 +25,12 @@
 #include "tiprimary_list.c" /* source required for CODA */
 #include "sdLib.h"
 #include "faV3Lib.h"        /* library of FADC250 routines */
+#include "faV3-HallD.h"     /* Hall D firmware */
 #include "faV3Config.h"
 
 #define BUFFERLEVEL 1
 
 /* FADC Library Variables */
-extern uint32_t faV3A32Base;
 extern int nfaV3;
 
 
@@ -39,7 +39,7 @@ extern int nfaV3;
 #define FADC_ADDR (3<<19)
 /* Increment address to find next fADC250 */
 #define FADC_INCR (1<<19)
-#define FADC_BANK 0x3
+#define FADC_BANK 0x250
 
 #define FAV3_READ_CONF_FILE {			\
     if(rol->usrConfig)				\
@@ -123,10 +123,11 @@ rocDownload()
 
   /* FADC Initialization flags */
   iflag = 0; /* NO SDC */
-  iflag |= (1<<0);  /* VXS sync-reset */
-  iflag |= FAV3_INIT_VXS_TRIG;  /* VXS trigger source */
-  iflag |= FAV3_INIT_VXS_CLKSRC;  /* VXS 250MHz Clock source */
+  iflag |= FAV3_INIT_EXT_SYNCRESET;  /* vxs sync-reset */
+  iflag |= FAV3_INIT_VXS_TRIG;       /* VXS trigger source */
+  iflag |= FAV3_INIT_INT_CLKSRC;     /* Internal 250MHz Clock source, switch to VXS in prestart */
 
+  extern uint32_t faV3A32Base;
   faV3A32Base = 0x09000000;
 
   vmeSetQuietFlag(1);
@@ -146,12 +147,6 @@ rocDownload()
     {
       /* Bus errors to terminate block transfers (preferred) */
       faV3EnableBusError(faV3Slot(ifa));
-
-      /*trigger-related*/
-      faV3SetTrigOut(faV3Slot(ifa), 7);
-
-      /* Enable busy output when too many events are being processed */
-      faV3SetTriggerBusyCondition(faV3Slot(ifa), 3);
     }
 
   sdSetActiveVmeSlots(faV3ScanMask()); /* Tell the sd where to find the fadcs */
@@ -170,12 +165,15 @@ rocPrestart()
   int ifa;
 
   /* Program/Init VME Modules Here */
+  /* Set Clock Source to VXS */
+  faV3GSetClockSource(2);
+  faV3GEnableSyncSrc();
+
   for(ifa=0; ifa < nfaV3; ifa++)
     {
       faV3SoftReset(faV3Slot(ifa),0);
       faV3ResetToken(faV3Slot(ifa));
       faV3ResetTriggerCount(faV3Slot(ifa));
-      faV3EnableSyncReset(faV3Slot(ifa));
     }
 
   /* Set number of events per block (broadcasted to all connected TI Slaves)*/
@@ -192,9 +190,6 @@ rocPrestart()
 void
 rocGo()
 {
-  int fadc_mode = 0;
-  uint32_t pl=0, ptw=0, nsb=0, nsa=0, np=0;
-
   /* Get the current block level */
   blockLevel = tiGetCurrentBlockLevel();
   printf("%s: Current Block Level = %d\n",
@@ -203,8 +198,12 @@ rocGo()
   faV3GSetBlockLevel(blockLevel);
 
   /* Get the FADC mode and window size to determine max data size */
-  faV3GetProcMode(faV3Slot(0), &fadc_mode, &pl, &ptw,
-		  &nsb, &nsa, &np);
+  int fadc_mode = 0;
+  uint32_t pl=0, ptw=0, nsb=0, nsa=0, np=0, nped=0, maxped=0, nsat=0;
+
+  faV3HallDGetProcMode(0, &fadc_mode, &pl, &ptw,
+		       &nsb, &nsa, &np,
+		       &nped, &maxped, &nsat);
 
   /* Set Max words from fadc (proc mode == 1 produces the most)
      nfaV3 * ( Block Header + Trailer + 2  # 2 possible filler words
@@ -216,7 +215,7 @@ rocGo()
   MAXFADCWORDS = nfaV3 * (4 + blockLevel * (4 + 16 * (1 + (ptw / 2))) + 18);
 
   /*  Enable FADC */
-  faV3GEnable(0, 0);
+  faV3GEnable(0);
 
   /* Interrupts/Polling enabled after conclusion of rocGo() */
 }
