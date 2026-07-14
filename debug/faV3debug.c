91eba07db5cb83fbb040ce3810a3f21d2555ec55
Bryan Moffit <moffit@jlab.org>
2026-07-14 19:37:09 -0400

    add routines to support dma, threaded readout

--- debug/faV3debug.c
+++ debug/faV3debug.c
@@ -16,6 +16,7 @@
 #include <limits.h>
 #include "toBin.h"
 #include "jvme.h"
+#include "dmaPList.h"
 #include "faV3Lib.h"
 #include "faV3-Compton.h"
 #include "faV3Config.h"
@@ -607,6 +608,118 @@ decode(char *choice)
 
   return 0;
 }
+
+#define MAX_EVENT_POOL     1
+#define MAX_EVENT_LENGTH   1024*20      /* Size in Bytes */
+DMA_MEM_ID vmeIN,vmeOUT;
+int32_t MAXFADCWORDS = 10*1024;
+
+int32_t
+dma_init()
+{
+  /* Setup Address and data modes for DMA transfers
+   *
+   *  vmeDmaConfig(addrType, dataType, sstMode);
+   *
+   *  addrType = 0 (A16)    1 (A24)    2 (A32)
+   *  dataType = 0 (D16)    1 (D32)    2 (BLK32) 3 (MBLK) 4 (2eVME) 5 (2eSST)
+   *  sstMode  = 0 (SST160) 1 (SST267) 2 (SST320)
+   */
+  vmeDmaConfig(2,5,1);
+
+  /* INIT dmaPList */
+
+  dmaPFreeAll();
+  vmeIN  = dmaPCreate("vmeIN",MAX_EVENT_LENGTH,MAX_EVENT_POOL,0);
+  vmeOUT = dmaPCreate("vmeOUT",0,0,0);
+
+  dmaPStatsAll();
+
+  dmaPReInitAll();
+
+  return 0;
+}
+
+int32_t
+dma_close()
+{
+  dmaPFreeAll();
+  return 0;
+}
+
+void
+readout_loop(void)
+{
+  extern DMANODE *the_event;
+  extern unsigned int *dma_dabufp;
+
+  while(1)
+    {
+      int ready_count = 0;
+      pthread_testcancel();
+      int32_t bready = faV3Bready(faV3Slot(0));
+
+      if(bready)
+	{
+	  GETEVENT(vmeIN, ++ready_count);
+	  int32_t nwords = faV3ReadBlock(faV3Slot(0), dma_dabufp, MAXFADCWORDS, 1);
+
+	  /* Check for ERROR in block read */
+	  int32_t blockError = faV3GetBlockError(1);
+	  faV3ResetToken(faV3Slot(0));
+
+	  if(blockError)
+	    {
+	      // break?
+	    }
+
+	  // increment counters, data size
+
+	  PUTEVENT(vmeOUT);
+	  DMANODE *outEvent = dmaPGetItem(vmeOUT);
+	  dmaPFreeItem(outEvent);
+	}
+    }
+
+  pthread_exit(0);
+
+}
+
+
+pthread_t      readout_thread;
+
+void
+start_readout_thread(void)
+{
+  int status;
+
+  status = pthread_create(&readout_thread, NULL,
+			  (void*(*)(void *)) readout_loop, (void *)NULL);
+  if(status!=0)
+    {
+      perror("pthread_create");
+
+      printf("%s: ERROR: Readout Thread could not be started.\n",
+	     __func__);
+      printf("\t pthread_create returned: %d\n", status);
+    }
+
+}
+
+void
+cancel_readout_thread(void)
+{
+  int status = 0;
+  if(readout_thread)
+    {
+      if(pthread_cancel(readout_thread)<0)
+	perror("pthread_cancel");
+      if(pthread_join(readout_thread, NULL)<0)
+	perror("pthread_join");
+    }
+
+}
+
 
 int32_t
 showbin(char *choice)
